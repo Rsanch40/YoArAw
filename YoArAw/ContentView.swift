2c76aa123ddc8416da94a0659c0861b8d73f4b68
Rafael <rsanch40@uic.edu>
2026-01-13 13:32:04 -0600

    Initial Commit

---
+++ YoArAw/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YoArAw
+//
+//  Created by Sanchez, Rafael on 1/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YoArAw/YoArAwApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoArAwApp.swift
+//  YoArAw
+//
+//  Created by Sanchez, Rafael on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct YoArAwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
